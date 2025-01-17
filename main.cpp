4c49d72654d70484b42918b9e239e6afaa0c19dd
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-01-17 21:53:18 +1000

    upd: изменение выввода (визуал)

--- main.cpp
+++ main.cpp
@@ -16,16 +16,16 @@ int main(int argc, char** argv){
     auto V = std::make_unique<unsigned[]>(N);
     for(auto i = 0; i < N; ++i)
         V[i] = i;
-
-    cout << "simple_sum: " << speedtest(simple_sum, V.get(), N) << std::endl;
-    cout << "sum_with_omp_reduce: " << speedtest(sum_with_omp_reduce, V.get(), N) << std::endl;
-    cout << "sum_with_omp_round_robin: " << speedtest(sum_with_omp_round_robin, V.get(), N) << std::endl;
-    cout << "sum_with_aligned_omp_round_robin: " << speedtest(sum_with_aligned_omp_round_robin, V.get(), N) << std::endl;
-    cout << "simple_sum_with_aligned: " << speedtest(simple_sum_with_aligned, V.get(), N) << std::endl;
-    cout << "sum_with_omp_and_thread: " << speedtest(sum_with_omp_and_thread, V.get(), N) << std::endl;
-    cout << "sum_with_cpp_methons: " << speedtest(sum_with_cpp_methons, V.get(), N) << std::endl;
-    cout << "sum_with_omp_mutex: " << speedtest(sum_with_omp_mutex, V.get(), N) << std::endl;
-    cout << "sum_with_cpp_barrier: " << speedtest(sum_with_cpp_barrier, V.get(), N) << std::endl;
+    
+    cout << speedtest(simple_sum, V.get(), N) << "\t" << "simple_sum" << std::endl;
+    cout << speedtest(simple_sum_with_aligned, V.get(), N) << "\t" << "simple_sum_with_aligned" << std::endl;
+    cout << speedtest(sum_with_omp_reduce, V.get(), N) << "\t" << "sum_with_omp_reduce"  << std::endl;
+    cout << speedtest(sum_with_omp_round_robin, V.get(), N) << "\t" << "sum_with_omp_round_robin" << std::endl;
+    cout << speedtest(sum_with_aligned_omp_round_robin, V.get(), N) << "\t" <<"sum_with_aligned_omp_round_robin" << std::endl;
+    cout << speedtest(sum_with_omp_and_thread, V.get(), N) << "\t" << "sum_with_omp_and_thread" << std::endl;
+    cout << speedtest(sum_with_cpp_methons, V.get(), N) << "\t" << "sum_with_cpp_methons: " <<  std::endl;
+    cout << speedtest(sum_with_omp_mutex, V.get(), N) << "\t" << "sum_with_omp_mutex: " << std::endl;
+    // cout << "sum_with_cpp_barrier: " << speedtest(sum_with_cpp_barrier, V.get(), N) << "\t" << std::endl;
 
     return 0;
 }
