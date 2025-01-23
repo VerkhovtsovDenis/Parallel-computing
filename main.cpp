f7bfbddca4eca0e4dcaca9aba24aa82da3a3d7da
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-01-23 13:31:05 +1000

    рефакторинг: вычистка файла - удаление лишнего

--- main.cpp
+++ main.cpp
@@ -7,15 +7,6 @@
 #include "run_experements.hpp"
 
 int main(int argc, char** argv){
-    // set_num_threads(4);
-
-    using std::cin, std::cout, std::endl;
-    
-    // cout << "N = " << N << endl;
-
-    // auto V = std::make_unique<unsigned[]>(N);
-    // for(auto i = 0; i < N; ++i)
-    //     V[i] = i;
     
     for (auto &funk : functions){
         auto filename = ("output//" + std::string(funk.first) + std::string(".csv")).c_str(); 
@@ -27,7 +18,7 @@ int main(int argc, char** argv){
         for (auto &item : result){
             fprintf(file, p, item.T, item.match, item.time, item.speedup, item.efficiency);
         }
-        cout << funk.first << " is end \n";
+        std::cout << funk.first << " is end \n";
 
     }
     return 0;
